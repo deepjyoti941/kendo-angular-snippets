eee29bbabb20471a6a94aeda1638bb25f7211a95
Deepjyoti Khakhlary <deepjyoti941@gmail.com>
2016-05-01 23:11:59 +0530

    kendo basic select and multiselect with angularjs

---
+++ src/app/welcome/welcome.js
@@ -0,0 +1,44 @@
+(function() {
+	'use strict'
+
+	angular.module('app').
+		controller('welcome', [welcome]);
+
+		function welcome () {
+			var vm = this;
+
+			vm.computers = [
+				{name: "Macbook Pro", id: 1},
+				{name: "Macbook Air", id: 2},
+				{name: "Mac Pro", id: 3}
+			];
+			vm.selectChange = function(e){
+				console.log(e.sender.text());
+				console.log(e.sender.value());
+			}
+
+
+
+    	vm.selectedIds = [];
+	    vm.selectOptions = {
+        placeholder: "Select products...",
+        dataTextField: "ProductName",
+        dataValueField: "ProductID",
+        dataSource: {
+            type: "odata",
+            serverFiltering: true,
+            transport: {
+                read: {
+                    url: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Products",
+                }
+            }
+        }
+    	};
+    	vm.multiSelectChange = function(e) {
+				console.log(e.sender.value());
+    	}
+
+
+
+		}
+})();
